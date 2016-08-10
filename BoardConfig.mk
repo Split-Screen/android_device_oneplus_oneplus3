958e69ffc8897749c088dcf133c881f6ce94d8f8
Naman Bhalla <namanbhalla1998@gmail.com>
2016-08-10 19:13:34 +0200

    oneplus3: Fix Build
    Change-Id: Ibedcbdac537dc7ba3bcdda89b5de1c72f9e44fe8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,6 +184,17 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/qcom/common
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+RECOVERY_VARIANT := twrp
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_CRYPTO := true
+TW_DEFAULT_BRIGHTNESS := 80
+TW_INCLUDE_NTFS_3G := true
+BOARD_HAS_LARGE_FILESYSTEM := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
